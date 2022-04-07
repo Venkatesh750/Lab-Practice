6f3e332c35e84fc7f4e7b9c05c17deaa5cfc2c17
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-07 16:27:27 +0530

    Add files via upload

---
+++ String_Subtring_startindex_length/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* C# program to demonstrate the String.Substring Method (int startIndex, int length) */
+            string value;
+            Console.Write("Enter the value for string : ");
+            value = Console.ReadLine();
+            Console.WriteLine("\nSub String for {0} is {1}", value, value.Substring(0, 3));
+
+            Console.ReadLine();
+        }
+    }
+}
